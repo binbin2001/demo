81bec131fa9dd63d84e301e9e4949cf5218a0f67
binbin2001 <thanhbinhpro785@gmail.com>
2021-10-19 10:21:05 +0700

    123111

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
 app.set('view engine','hbs')
-
+app.use(express.urlencoded({extended:true}))
 app.get('/',(req,res)=>{
     res.render('home')
 })
-
+app.post('/insert',(req,res)=>{
+    const Name = req.body.txtName;
+    const Price = req.body.txtPrice;
+    const Size = req.body.value="Big", value="Small", value="Medium";
+    const Category = req.body.value="Búp bê và thú bông", value="Đồ chơi mô hình", value="Lego", value="mô hình";
+    //goi ham de insert vao database
+    
+//    const Imgine = req.body.
+})
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
 console.log('Bin_Shop is activity!!!!')
---
+++ databaseHandler.js
@@ -0,0 +1,4 @@
+const {MongoClient} = require('mongodb')
+
+const DATABASE_URL = 'mongodb+srv://thanhbinh2001:123456789abc@cluster0.spshl.mongodb.net/test'
+
